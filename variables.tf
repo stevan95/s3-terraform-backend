c9e183de9d333a6bb4290acd53f9cacc41d5679e
Stevan <mstevan95@gmail.com>
2022-09-11 12:35:44 +0200

    Removed role definition

--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@ variable "namespace" {
     type = string
 }
 
-variable "principal_arns" {
-    description = "A list of principal arns allowed to assume the IAM role"
-    default = null
-    type = list(string)
-}
-
 variable "force_destroy_state" {
     description = "Force destroy the s3 bucket containing state files?"
     default = true
